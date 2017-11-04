bc52c63dab6edfcd963ed681f1765502f29da7ba
atnafudargaso <atnafudargaso@atnafus-mbp>
2017-11-04 02:11:34 -0700

    Test repo

---
+++ Collections/list/list/list.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package list;
+
+import java.util.*;
+
+/**
+ * @author atnafudargaso
+ *
+ */
+public class list {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class ArrayList {
+	String name=null;
+     //List<String> lst=new ArrayList<String>();
+}
